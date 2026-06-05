f0aaa8b5209114d5fe9945da6b81d0362479c26e
alvaro <acepero13@gmail.com>
2026-06-05 16:10:43 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk          = 26
         targetSdk       = 35
         versionCode     = 1
-        versionName     = "2.7.24"
+        versionName     = "2.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
