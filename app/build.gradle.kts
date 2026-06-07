9ea8252c3b776519c7e721b4422b6d1f18a557aa
alvaro <acepero13@gmail.com>
2026-06-07 08:59:06 +0200

    bump version to 2.9.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk          = 26
         targetSdk       = 35
         versionCode     = 1
-        versionName     = "2.8.10"
+        versionName     = "2.9.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
